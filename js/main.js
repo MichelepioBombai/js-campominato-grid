2435fb1ce4bdebb925028876b4af11989a3e398e
MichelepioBombai <michelebombai@icloud.com>
2023-02-02 11:01:05 +0100

    fixed function this

--- js/main.js
+++ js/main.js
@@ -1,60 +1,69 @@
+
+// Bonus
+// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+
+
+
 /************************************
-*                                   *
-*              ON LOAD              *
-*                                   *
+ *                                   *
+ *              ON LOAD              *
+ *                                   *
 ************************************/
 
 
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 const startGame = document.getElementById("buttonStart")
-
-
 
 startGame.addEventListener(
     "click",
     function() {
-
+        
         const gridEl = document.getElementById("grid")
-
+        
         const gridDimension = 100
-        generateGrid(gridEl, gridDimension);
-
+        const gridGenerator = generateGrid(gridEl, gridDimension);
+        
     }
-
-)
-
-
-
-/************************************
-*                                   *
-*            FUNCTIONS              *
-*                                   *
-************************************/
-
-function generateGrid(grid, dimension) {
-
-    grid.innerHTML = "";
-
-
+    
+    )
     
     
-    for(let i = 1; i <= 100; i++){
-        
-        // genero un div
-        const squareEl = document.createElement("div");
-        
-        // aggiungo la classe .square
-        squareEl.classList.add("square");
-        
-        
-        // assegno un valore da 1-100 ad ogni cella
-        squareEl.innerHTML = i;
-        
-        // aggiungo addeventlistener
+    
+    /************************************
+     *                                   *
+     *            FUNCTIONS              *
+     *                                   *
+    ************************************/
+   
+   function generateGrid(gridEl, gridDimension) {
+       
+       grid.innerHTML = ""; 
+       
+       
+       // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+       for(let i = 1; i <= 100; i++){
+           
+           // genero un div
+           const squareEl = document.createElement("div");
+           
+           // aggiungo la classe .square
+           squareEl.classList.add("square");
+           
+           
+           // assegno un valore da 1-100 ad ogni cella
+           squareEl.innerHTML = i;
+           
+         
+           // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
         squareEl.addEventListener(
             "click",
-            function () {
-                console.log(this);
-                this.classlist.toggle("active");
+            function() {
+                console.log(this.innerHTML);
+                this.classList.toggle("active");
             }
         )
         // lo aggiungo alla griglia
